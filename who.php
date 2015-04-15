e843c0a3f0c68c229c1f04e7f7f39f6fb33b2fcc
nicoleee <i7752205@bournemouth.ac.uk>
2015-04-15 09:30:22 +0100

    Linked image to photo booth page

--- who.php
+++ who.php
@@ -22,9 +22,11 @@ include './parts/header.php';
 		<img src="media/who-graphic.png" alt="Motion Graphic">
 	</div>
 
-	<div class="third media-holder left pad-image">
+	<a href="https://www.facebook.com/media/set/?set=a.329196940624028.1073741828.329195953957460&type=3">
+		<div class="third media-holder left pad-image">
 		<img src="media/who-photobooth.png" alt="Photobooth">
 	</div>
+</a>
 
 </div>
 
