e8df603067d20032bf370d937323aad412d96914
nicoleee <i7752205@bournemouth.ac.uk>
2015-04-16 23:32:53 +0100

    Testing text and image on how page

--- how.php
+++ how.php
@@ -7,10 +7,40 @@ include './parts/header.php';
  <div class="container">
 	<h1 class="h1 align-center text--purple">how vendible works</h1>
 
-	<p class="left">This video displays how Vendible products work in a small vintage store. Our aim is to give customers an experience, making their time
-		physically shopping memorable. This is something that is not possible online.</p>
+	<p class="left">This video displays how Vendible products work in a small vintage store. Our aim is to give customers an experience, 
+		making their time physically shopping memorable. This is something that is not possible online.</p>
 </div>
+
+<div class="container">
+
+<img src="media/mockshop.jpg">
+
+<p class="left half">
+	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vestibulum sapien pharetra ipsum ornare, 
+	nec sagittis erat tincidunt. Ut a mi nibh. Phasellus laoreet pharetra nunc, mattis pretium neque fermentum ac. 
+	Nullam non est ac turpis euismod congue non eu eros. Pellentesque consequat ullamcorper erat, ut sagittis enim suscipit vel. 
+	Nullam eu vulputate lacus, nec venenatis erat. Donec nisi est, auctor vitae viverra at, porta vitae libero. Aenean sed porttitor 
+	tortor. Donec iaculis, massa efficitur dignissim fringilla, elit turpis tristique leo, venenatis blandit justo dolor quis magna. 
+	Nulla eget arcu urna. Etiam mattis erat non neque efficitur, id scelerisque mi facilisis. tortor. Donec iaculis
+</p>
+<img src="media/piechart.jpg" class="half right pad-image">
 </div>
+
+<div class="container">
+<p class="right half">
+	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vestibulum sapien pharetra ipsum ornare, 
+	nec sagittis erat tincidunt. Ut a mi nibh. Phasellus laoreet pharetra nunc, mattis pretium neque fermentum ac. 
+	Nullam non est ac turpis euismod congue non eu eros. Pellentesque consequat ullamcorper erat, ut sagittis enim suscipit vel. 
+	Nullam eu vulputate lacus, nec venenatis erat. Donec nisi est, auctor vitae viverra at, porta vitae libero. Aenean sed porttitor 
+	tortor. Donec iaculis, massa efficitur dignissim fringilla, elit turpis tristique leo, venenatis blandit justo dolor quis magna. 
+	Nulla eget arcu urna. Etiam mattis erat non neque efficitur, id scelerisque mi facilisis.
+</p>
+
+<img src="media/how-window.jpg" class="half left pad-image">
+</div>
+
+</div>
+
 
  <?php
 
