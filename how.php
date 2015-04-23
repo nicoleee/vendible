2643a3a654d49b9a7f9daa63f1c195279227eea7
nicoleee <i7752205@bournemouth.ac.uk>
2015-04-23 20:34:30 +0100

    Changed content of How page

--- how.php
+++ how.php
@@ -7,13 +7,16 @@ include './parts/header.php';
  <div class="container">
 	<h1 class="h1 align-center text--purple">how vendible works</h1>
 
+	<img src="media/info-poster.jpg"
+
 	<p class="left">Our aim is to give customers an experience, making their time physically shopping memorable. 
 		This is something that is not possible online. Vendible products are interactive and have a place in any shop.</p>
+
+	<img src="media/how.png">
+
 </div>
 
 <div class="container">
-
-<img src="media/mockshop.jpg">
 
 <p class="left half">
 	Over 73% of people said yes or maybe when asked the question, "Would an interactive art installation in a shop, make you more likely 
@@ -29,21 +32,6 @@ include './parts/header.php';
 </p>
 
 <img src="media/piechart.jpg" class="half right pad-image">
-</div>
-
-<div class="container">
-<p class="right half">
-	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vestibulum sapien pharetra ipsum ornare, 
-	nec sagittis erat tincidunt. Ut a mi nibh. Phasellus laoreet pharetra nunc, mattis pretium neque fermentum ac. 
-	Nullam non est ac turpis euismod congue non eu eros. Pellentesque consequat ullamcorper erat, ut sagittis enim suscipit vel. 
-	Nullam eu vulputate lacus, nec venenatis erat. Donec nisi est, auctor vitae viverra at, porta vitae libero. Aenean sed porttitor 
-	tortor. Donec iaculis, massa efficitur dignissim fringilla, elit turpis tristique leo, venenatis blandit justo dolor quis magna. 
-	Nulla eget arcu urna. Etiam mattis erat non neque efficitur, id scelerisque mi facilisis.
-</p>
-
-<img src="media/how-window.jpg" class="half left pad-image">
-</div>
-
 </div>
 
 
