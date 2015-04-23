d7d8fe4d892afe14a6af2fccf3830acf07ba8c21
nicoleee <i7752205@bournemouth.ac.uk>
2015-04-23 20:02:33 +0100

    Added content to How page

--- how.php
+++ how.php
@@ -7,8 +7,8 @@ include './parts/header.php';
  <div class="container">
 	<h1 class="h1 align-center text--purple">how vendible works</h1>
 
-	<p class="left">This video displays how Vendible products work in a small vintage store. Our aim is to give customers an experience, 
-		making their time physically shopping memorable. This is something that is not possible online.</p>
+	<p class="left">Our aim is to give customers an experience, making their time physically shopping memorable. 
+		This is something that is not possible online. Vendible products are interactive and have a place in any shop.</p>
 </div>
 
 <div class="container">
@@ -16,13 +16,18 @@ include './parts/header.php';
 <img src="media/mockshop.jpg">
 
 <p class="left half">
-	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vestibulum sapien pharetra ipsum ornare, 
-	nec sagittis erat tincidunt. Ut a mi nibh. Phasellus laoreet pharetra nunc, mattis pretium neque fermentum ac. 
-	Nullam non est ac turpis euismod congue non eu eros. Pellentesque consequat ullamcorper erat, ut sagittis enim suscipit vel. 
-	Nullam eu vulputate lacus, nec venenatis erat. Donec nisi est, auctor vitae viverra at, porta vitae libero. Aenean sed porttitor 
-	tortor. Donec iaculis, massa efficitur dignissim fringilla, elit turpis tristique leo, venenatis blandit justo dolor quis magna. 
-	Nulla eget arcu urna. Etiam mattis erat non neque efficitur, id scelerisque mi facilisis. tortor. Donec iaculis
+	Over 73% of people said yes or maybe when asked the question, "Would an interactive art installation in a shop, make you more likely 
+	to physically shop?"
+</br></br>
+
+	Modern society is used to living in a digital world. Online shopping is more convenient and easier for a lot of civilisation. Vendible products
+	give the public an experience which is not possible online. This will change the nature of physically shopping and encourage society to go
+	back to visiting the high street.
+</br></br>
+
+    This is what makes Vendible perfect for small stores with or without an online presence.
 </p>
+
 <img src="media/piechart.jpg" class="half right pad-image">
 </div>
 
