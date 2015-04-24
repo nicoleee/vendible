aff07b831051cf0cd24aaba1ec8215a22429d4a2
nicoleee <i7752205@bournemouth.ac.uk>
2015-04-24 13:21:01 +0100

    Changed content on Who page

--- who.php
+++ who.php
@@ -4,15 +4,9 @@ include './parts/header.php';
 
  ?>
 
- <div class="container">
-	<h1 class="h1 align-center text--purple">who's it for?</h1>
+ <div class="section container">
 
-	<p class="left">Vendible offers visual marketing products for shop windows, stores and festivals. They are perfect to make your store stand out and 
-		be remembered. A wide range of playful window transfers, motion graphics and photo booths. Premade themed products are ideal for
-		Easter, Christmas or Halloween.</p>
-</div>
-
-<div class="section container">
+ <h1 class="h1 align-center text--purple">who's it for?</h1>
 
 <a href="https://www.facebook.com/media/set/?set=a.329707043906351.1073741830.329195953957460&type=1">
 	<div class="third media-holder left pad-image">
@@ -24,13 +18,33 @@ include './parts/header.php';
 		<img src="media/who-graphic.png" alt="Motion Graphic">
 	</div>
 
-	<a href="https://www.facebook.com/media/set/?set=a.329196940624028.1073741828.329195953957460&type=3">
-		<div class="third media-holder left pad-image">
+<a href="https://www.facebook.com/media/set/?set=a.329196940624028.1073741828.329195953957460&type=3">
+	<div class="third media-holder left pad-image">
 		<img src="media/who-photobooth.png" alt="Photobooth">
 	</div>
 </a>
 
 </div>
+
+ <div class="container">
+	
+
+	<p class="left">
+		Vendible is a service aimed at shop owners, event organisers and artists. The visual marketing products available are designed
+		to make shopping an experience that is not available online.
+	</br></br>
+
+		There is so much choice on the high street, it is important that your company stands out. Vendible products are remembered and customers
+		will return. We offer a wide range of playful window transfers, motion graphics and photo booth props. 
+		Premade kits are ideal for Easter, Christmas or Halloween.
+	</br></br>
+
+		Vendible is unique for many reasons. One being that we aim to not only help independent stores, but also artists. Vendible offers an
+		opportunity for artists to submit their work and be used by our buyers. Vendible believes it is important to promote fresh talent and keep up 
+		to date with all styles.
+	</p>
+</div>
+
 
  <?php
 
