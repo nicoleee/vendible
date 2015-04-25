c6fb10f5b8821db6e24b7c23d5fb18c80953e375
nicoleee <i7752205@bournemouth.ac.uk>
2015-04-25 14:32:34 +0100

    Content changed on Join In page

--- join.php
+++ join.php
@@ -7,8 +7,15 @@ include './parts/header.php';
  <div class="container">
 	<h1 class="h1 align-center text--purple">join in</h1>
 
-	<p class="left">Could you see your designs being part of a Vendible kit? Do you fancy displaying your creations in a different way? If you want to collaborate with Vendible and get your artwork recognised please <a href="contact.php">contact</a> us. We 
-		welcome all types of artists, take a look at our current collaborators.</p>
+	<p class="left">
+		Vendible offers a unique service which aims to help artists as well as business owners. By collaborating with a variety of up
+		and coming artists we are offering different styles as well as giving artists an opportunity to get noticed.
+	</br></br>
+		Could you see your designs being part of a Vendible kit? Do you fancy displaying your creations in a different way? If you 
+	 	to collaborate with Vendible and get your artwork recognised please <a href="contact.php">contact</a> us. We 
+		welcome all types of artists, take a look at our current collaborators.
+	</p>
+	
 </div>
 
 <div class="container quarter">
