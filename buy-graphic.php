812ed57b369468b340c3bb0eba8a3bdc9f8249b2
nicoleee <i7752205@bournemouth.ac.uk>
2015-04-29 23:26:38 +0100

    Added buy button and image

--- buy-graphic.php
+++ buy-graphic.php
@@ -4,7 +4,18 @@ include './parts/header.php';
 
  ?>
 
- 
+  <div class="container">
+ 	
+ 	<h1 class="h1 align-center text--purple">motion graphics</h1>
+
+ 	<a href="#">
+	<p class="btn--purple-outline align-center third">
+		Buy
+	</p></a>
+
+	<img src="media/who-graphic.png" class="third">
+
+ </div>
 
  <?php
 
--- buy-kit.php
+++ buy-kit.php
@@ -4,7 +4,18 @@ include './parts/header.php';
 
  ?>
 
- 
+  <div class="container">
+ 	
+ 	<h1 class="h1 align-center text--purple">premade kits</h1>
+
+ 	<a href="#">
+	<p class="btn--purple-outline align-center third">
+		Buy
+	</p></a>
+
+	<img src="media/premade2.png" class="third">
+
+ </div>
 
  <?php
 
