73fb5d394b88773ac3b5f0833d1a13ed81287a1f
nicoleee <i7752205@bournemouth.ac.uk>
2015-04-30 12:46:01 +0100

    Product page changed
    No longer hover over

--- products.php
+++ products.php
@@ -24,35 +24,34 @@ Buttons on the left
     <div class="product-wrap">
       <a href="buy-window.php">
         <p class="btn--purple-outline">Window Transfers</p>
+        <div class="product">
+          <div class="product-img third left">
+            <img src="media/prod-window.png" alt="" />
+          </div>
+          <div class="product-description two-thirds left">
+            <p>Window transfers are a fun and interactive way to give customers an experience. Adults and kids love taking photographs 
+              with the transfers. They are super easy to apply and remove, making them an ideal tool for keeping window displays up to date.
+              They are perfect for any shop window.</p>
+          </div>
+        </div>
       </a>
-      <div class="product">
-        <div class="product-img third left">
-          <img src="media/prod-window.png" alt="" />
-        </div>
-        <div class="product-description two-thirds left">
-          <p>Window transfers are a fun and interactive way to give customers an experience. Adults and kids love taking photographs 
-            with the transfers. They are super easy to apply and remove, making them an ideal tool for keeping window displays up to date.
-            They are perfect for any shop window.</p>
-        </div>
-      </div>
     </div>
 
     <div class="product-wrap">
-      <a href="buy-graphic.php">
+      <a href="buy-window.php">
         <p class="btn--purple-outline">Motion Graphics</p>
-      </a>
-      <div class="product">
-        <div class="product-img third left">
-          <img src="media/prod-graphic.png" alt="" />
-        </div>
-        <div class="product-description two-thirds left">
-          <p>Motion graphics are modern and playful. They can be easily updated to display new offers or promotions. Having an eye catching
-            motion graphic will attract customers and get promotions noticed. They are also the perfect way to display your shops branding. 
-            Ditch the boring signs and get yourself a motion graphic.</p>
+        <div class="product">
+          <div class="product-img third left">
+            <img src="media/prod-graphic.png" alt="" />
+          </div>
+          <div class="product-description two-thirds left">
+            <p>Motion graphics are modern and playful. They can be easily updated to display new offers or promotions. Having an eye catching
+              motion graphic will attract customers and get promotions noticed. They are also the perfect way to display your shops branding. 
+              Ditch the boring signs and get yourself a motion graphic.</p>
+          </div>
         </div>
       </div>
-    </div>
-
+    </a>
   </div>
 
 <!--
@@ -62,35 +61,36 @@ Buttons on the right
   <div class="left half pad-image">
 
     <div class="product-wrap">
-      <a href="buy-photo.php">
+      <a href="buy-window.php">
         <p class="btn--purple-outline">Photo Booths</p>
+        <div class="product">
+          <div class="product-img third left">
+            <img src="media/photobooth.png" alt="" />
+          </div>
+          <div class="product-description two-thirds left">
+            <p>Photo booths are ideal for groups! Customers remember their experience and share the photographs on social media, making them 
+              models for your shop overnight.
+            </br>
+              Choose frames and props to match your shop's style.</p>
+          </div>
+        </div>
       </a>
-      <div class="product">
-        <div class="product-img third left">
-          <img src="media/photobooth.png" alt="" />
-        </div>
-        <div class="product-description two-thirds left">
-          <p>Photo booths are ideal for groups! Customers remember their experience and share the photographs on social media, making them 
-            models for your shop overnight. 
-            Choose frames and props to match your shop's style.</p>
-        </div>
-      </div>
     </div>
 
     <div class="product-wrap">
-      <a href="buy-kit.php">
+      <a href="buy-window.php">
         <p class="btn--purple-outline">Premade Kits</p>
+        <div class="product">
+          <div class="product-img third left">
+            <img src="media/premade.png" alt="" />
+          </div>
+          <div class="product-description two-thirds left">
+            <p>Vendible premade kits are ideal for holidays and events. Keeping your store up to date is important. Vendible 
+              makes this easier for you with premade marketing kits. They include transfers, photo booth props and a motion graphic. 
+              Themed for holidays like Halloween and Christmas. Transforming your shop instantly.</p>
+          </div>
+        </div>
       </a>
-      <div class="product">
-        <div class="product-img third left">
-          <img src="media/premade.png" alt="" />
-        </div>
-        <div class="product-description two-thirds left">
-          <p>Vendible premade kits are ideal for holidays and events. Keeping your store up to date is important. Vendible 
-            makes this easier for you with premade marketing kits. They include transfers, photo booth props and a motion graphic. 
-            Themed for holidays like Halloween and Christmas. Transforming your shop instantly.</p>
-        </div>
-      </div>
     </div>
 
   </div>
